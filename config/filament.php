e6ec0152b9828fa5d2779f992cc317f62744d71c
Tiago França <dev@tiagofranca.com>
2023-06-02 14:02:58 -0300

    On branch tiago-v1.x Changes to be committed: 	modified:   config/filament.php 	modified:   resources/views/vendor/filament/components/footer.blade.php

--- config/filament.php
+++ config/filament.php
@@ -140,7 +140,6 @@ return [
         'path' => app_path('Filament/Widgets'),
         'register' => [
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
         ],
     ],
 
@@ -169,7 +168,7 @@ return [
     |
     */
 
-    'dark_mode' => false,
+    'dark_mode' => true,
 
     /*
     |--------------------------------------------------------------------------
